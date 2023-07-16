8c99b731a3fd263b416867eb7ccbcf4a93c6ee11
Ursula Babeto <ursulacobabeto@gmail.com>
2023-07-16 20:46:46 -0300

    adding stylesheet

--- src/components/ToolbarComponent/ToolbarComponent.jsx
+++ src/components/ToolbarComponent/ToolbarComponent.jsx
@@ -1,5 +1,15 @@
-function Toobar() {
-    return ( <></> );
+function ToobarComponent({ name, avatar, titulo }) {
+  return (
+    <>
+      <header>
+        <div>
+          <h2>{titulo}</h2>
+        </div>
+        <h3>Bem vindo, {name}</h3>
+        <img src={avatar} alt="foto pessoal" />
+      </header>
+    </>
+  );
 }
 
-export default Toobar;
+export default ToobarComponent;
