8add867dca0d86bfc8b51960664aa3d6720037f2
Ursula Babeto <ursulacobabeto@gmail.com>
2023-07-17 18:59:49 -0300

    initial config

--- src/components/CardPacienteComponent/CardPacienteComponent.jsx
+++ src/components/CardPacienteComponent/CardPacienteComponent.jsx
@@ -1,7 +1,17 @@
+import * as Styled from './CardPacienteStyled'
+
 function CardPacienteComponent() {
     return ( 
     <>
-    is render
+    <Styled.Div className="card">
+        <Styled.Image src="" alt="" />
+       
+        <span>nome</span>
+        <span>idade</span>
+        <span>telefone</span>
+        <span>plano</span>
+        <a href="">ver mais</a>
+    </Styled.Div>
     </> );
 }
 
---
+++ src/components/CardPacienteComponent/CardPacienteStyled.js
@@ -0,0 +1,21 @@
+import { styled } from "styled-components";
+
+export const Div = styled.div`
+margin: 8px;
+width: 305px;
+height: 295px;
+background-color: #FFF;
+display: flex;
+flex-direction: column;
+align-items: center;
+box-shadow: 0 0 6px #A8a3dC;
+`
+
+export const Image = styled.img`
+width: 90px;
+height:90px;
+border-radius: 50%;
+background-color: #DEE2E6;
+background: cover;
+margin-top:10px ;
+`
