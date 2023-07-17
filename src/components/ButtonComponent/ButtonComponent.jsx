83545b909648383682f3be56dd3a71d621080cc0
Ursula Babeto <ursulacobabeto@gmail.com>
2023-07-16 22:39:45 -0300

    creating and styling login page

--- src/components/ButtonComponent/ButtonComponent.jsx
+++ src/components/ButtonComponent/ButtonComponent.jsx
@@ -1,6 +1,12 @@
 import * as Styled from "./ButtonStyled"
+import { useForm } from "react-hook-form";
 
 function ButtonComponent({nome}) {
+
+  const {
+    formState: { errors },
+  } = useForm();
+
     return ( <>
         <Styled.Button
           $active={!errors.password && !errors.email}
