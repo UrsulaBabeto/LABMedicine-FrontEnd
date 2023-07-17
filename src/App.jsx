bf0b7a2dfad8ff9fe31953c42255dea3e6d91716
Ursula Babeto <ursulacobabeto@gmail.com>
2023-07-16 22:48:46 -0300

    homepage setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-
-import './App.css'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import HomePage from './pages/HomePage/HomePage'
 
 
 function App() {
 
 
   return (
-    <>
+    <Router>
+    <Routes>
+       {/*  <Route path='/' element={<LoginPage/>}/> */}
+        <Route path='/home' element={<HomePage/>}/>
+{/*         <Route path='*' element={}/> */}
+    </Routes>
+ </Router>
 
-      app is render
-
-
-    </>
   )
 }
 
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import ToolbarComponent from "../../components/ToolbarComponent/ToolbarComponent";
+import NavbarComponent from "../../components/NavbarComponent/NavbarComponent";
+
+function HomePage() {
+    return (
+          <>
+          <header>
+            <ToolbarComponent/>
+            <NavbarComponent/>
+          </header>
+    </>);
+}
+
+export default HomePage;
