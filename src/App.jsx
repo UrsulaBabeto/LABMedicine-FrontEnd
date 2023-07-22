d0d0adc3598d7e03c76980467b7603136ea8dcbf
Ursula Babeto <ursulacobabeto@gmail.com>
2023-07-22 11:35:53 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
+
 import LoginPage from "./pages/LoginPage/LoginPage";
 import CadastroPacientePage from "./pages/CadastroPacientePage/CadastroPacientePage";
 import CadastroExamePage from "./pages/CadastroExamePage/CadastroExamePage";
