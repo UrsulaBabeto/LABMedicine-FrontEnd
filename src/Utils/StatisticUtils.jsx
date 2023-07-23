4086f6d2db361b52ed8251ff0045f09a586c5948
Ursula Babeto <ursulacobabeto@gmail.com>
2023-07-23 18:31:02 -0300

    statistic omponent

---
+++ src/Utils/StatisticUtils.jsx
@@ -0,0 +1,27 @@
+import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt';
+import BiotechIcon from '@mui/icons-material/Biotech';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+
+export default arrStatistic = [
+  {
+    id: 1,
+    icon: <PersonAddAltIcon/>,
+    title:"Pacientes" ,
+    table: "Pacientes",
+    value: "",
+  },
+  {
+    id: 2,
+    icon: <BiotechIcon/>,
+    title: "Exames",
+    table: "Exames",
+    value: "",
+  },
+  {
+    id: 3,
+    title: <CalendarMonthIcon/>,
+    table: "Consultas",
+    value: "Consultas",
+    value: "",
+  },
+];
